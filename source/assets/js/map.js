4e1af823f8a4a7b1c0b5bca7994087f7a66612c2
Jacob Lesser <jacob@jacoblesser.com>
2013-07-19 13:55:51 -0700

    Added state abbreviation lookup and modified the industry to display the state name instead of "in the state"

--- source/assets/js/map.js
+++ source/assets/js/map.js
@@ -12,6 +12,65 @@
 	var map;
 	var layers = {};
 	
+	var stateLookup = {
+		"AK":"Alaska",
+		"AL":"Alabama",
+		"AR":"Arkansas",
+		"AS":"American Samoa",
+		"AZ":"Arizona",
+		"CA":"California",
+		"CO":"Colorado",
+		"CT":"Connecticut",
+		"DC":"District of Columbia",
+		"DE":"Delaware",
+		"FL":"Florida",
+		"GA":"Georgia",
+		"GU":"Guam",
+		"HI":"Hawaii",
+		"IA":"Iowa",
+		"ID":"Idaho",
+		"IL":"Illinois",
+		"IN":"Indiana",
+		"KS":"Kansas",
+		"KY":"Kentucky",
+		"LA":"Louisiana",
+		"MA":"Massachusetts",
+		"MD":"Maryland",
+		"ME":"Maine",
+		"MI":"Michigan",
+		"MN":"Minnesota",
+		"MO":"Missouri",
+		"MP":"Northern Mariana Islands",
+		"MS":"Mississippi",
+		"MT":"Montana",
+		"NC":"North Carolina",
+		"ND":"North Dakota",
+		"NE":"Nebraska",
+		"NH":"New Hampshire",
+		"NJ":"New Jersey",
+		"NM":"New Mexico",
+		"NV":"Nevada",
+		"NY":"New York",
+		"OH":"Ohio",
+		"OK":"Oklahoma",
+		"OR":"Oregon",
+		"PA":"Pennsylvania",
+		"PR":"Puerto Rico",
+		"RI":"Rhode Island",
+		"SC":"South Carolina",
+		"SD":"South Dakota",
+		"TN":"Tennessee",
+		"TX":"Texas",
+		"UT":"Utah",
+		"VA":"Virginia",
+		"VI":"Virgin Islands",
+		"VT":"Vermont",
+		"WA":"Washington",
+		"WI":"Wisconsin",
+		"WV":"West Virginia",
+		"WY":"Wyoming"
+	}
+	
 	//Modify number prototype with format function to add commas and truncate long decimals
 	Number.prototype.format = function() {
 		try {
@@ -522,9 +581,9 @@
 					} else if (facility_record['NAICS' + i][windowYear].state_count === 1 && facility_record['NAICS' + i][windowYear].us_count === 1) {
 						html += 'There is only 1 facility of this type in the country.';
 					} else if (facility_record['NAICS' + i][windowYear].state_count === 1 && facility_record['NAICS' + i][windowYear].us_count > 1) {
-						html += 'There is only 1 facility of this type in the State. There are ' + facility_record['NAICS' + i][windowYear].us_count + ' in the country. It emits more pounds of chemicals than ' + facility_record['NAICS' + i][windowYear].us_count + ' percent of facilities of this type in the country.  The facility has a higher <a href="http://www.epa.gov/oppt/rsei/pubs/using_rsei.html#high_score" target="_blank">RSEI</a> risk score than ' + facility_record['NAICS' + i][windowYear].us_score_pct + ' percent of facilities of this type in the country.';
+						html += 'There is only 1 facility of this type in ' + stateLookup[facility_record['State']] + '. There are ' + facility_record['NAICS' + i][windowYear].us_count + ' in the country. It emits more pounds of chemicals than ' + facility_record['NAICS' + i][windowYear].us_count + ' percent of facilities of this type in the country.  The facility has a higher <a href="http://www.epa.gov/oppt/rsei/pubs/using_rsei.html#high_score" target="_blank">RSEI</a> risk score than ' + facility_record['NAICS' + i][windowYear].us_score_pct + ' percent of facilities of this type in the country.';
 					} else {
-						html += 'There are ' + facility_record['NAICS' + i][windowYear].state_count + ' facilities of this type in this state and ' + facility_record['NAICS' + i][windowYear].us_count + ' in the country. It emits more pounds of chemicals than ' + facility_record['NAICS' + i][windowYear].state_pounds_pct + ' percent of facilities of this type in the state and ' + facility_record['NAICS' + i][windowYear].us_pounds_pct + ' percent in the country. The facility has a higher <a href="http://www.epa.gov/oppt/rsei/pubs/using_rsei.html#high_score" target="_blank">RSEI</a> risk score than  ' + facility_record['NAICS' + i][windowYear].state_score_pct + ' percent of facilities of this type in the state and ' + facility_record['NAICS' + i][windowYear].us_score_pct + ' percent in the country.';
+						html += 'There are ' + facility_record['NAICS' + i][windowYear].state_count + ' facilities of this type in ' + stateLookup[facility_record['State']] + ' and ' +  facility_record['NAICS' + i][windowYear].us_count + ' in the country. It emits more pounds of chemicals than ' + facility_record['NAICS' + i][windowYear].state_pounds_pct + ' percent of facilities of this type in ' + stateLookup[facility_record['State']] + ' and ' + facility_record['NAICS' + i][windowYear].us_pounds_pct + ' percent in the country. The facility has a higher <a href="http://www.epa.gov/oppt/rsei/pubs/using_rsei.html#high_score" target="_blank">RSEI</a> risk score than  ' + facility_record['NAICS' + i][windowYear].state_score_pct + ' percent of facilities of this type in ' + stateLookup[facility_record['State']] + ' and ' + facility_record['NAICS' + i][windowYear].us_score_pct + ' percent in the country.';
 					}
 	
 					div.innerHTML = html;
