6c07b3d398c3dec42e282a4b52e3875709fc5d32
Jacob Lesser <jacob@jacoblesser.com>
2013-08-13 14:36:34 -0700

    encoded the "&"

--- source/assets/js/map.js
+++ source/assets/js/map.js
@@ -843,7 +843,7 @@
 		
 		// Social Share Listeners
 		$(".twitter").on("click", function(){
-			window.open("http://www.twitter.com/share?url=http%3A%2F%2Ftoxictrends.org&text=Check%20out%20the%20Toxic%20Trends%20Mapper%20where%20you%20can%20view%20the%20emissions%20of%20facilities%20in%20your%20neighborhood",'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=250,width=585');
+			window.open("http://www.twitter.com/share?url=http%3A%2F%2Ftoxictrends.org&amp;text=Check%20out%20the%20Toxic%20Trends%20Mapper%20where%20you%20can%20view%20the%20emissions%20of%20facilities%20in%20your%20neighborhood",'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=250,width=585');
 			ga('send', 'social', 'twitter', 'share', 'socialTarget', {'page': '/'});
 		})
 		
